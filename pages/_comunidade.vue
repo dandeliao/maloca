fb070ce71cdec97c53a8de149927bc8bc6a34f3a
Dandelião <danielramos@tuta.io>
2021-10-04 15:50:41 -0300

    feat: cria página dinâmica de comunidades

---
+++ pages/_comunidade.vue
@@ -0,0 +1,15 @@
+<template>
+  <main v-html="comuna">
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      comuna:
+        '<div><h1 style="font-size:32px">Abro a geladeira pra pensar</h1><Botao></Botao><h3 style="font-size:20px">Comunidade para todes que abrem a geladeira sem saber por que e aproveitam esse momento pra refletir sobre a vida</h3><p>Olá, pessoal!</p></div>',
+    }
+  },
+}
+</script>
