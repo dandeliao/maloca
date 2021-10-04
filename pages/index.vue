3749f74cd090e6887b94db77b261488aafb6457a
Dandelião <danielramos@tuta.io>
2021-10-04 15:51:34 -0300

    fix: insere elementos direto na página inicial

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
-    <Bar />
-    <Maloca />
-    <Footer />
-  </div>
+  <main>
+    <h2>Pessoas:</h2>
+    <NuxtLink to="pessoa/dani">dani</NuxtLink><br>
+    <NuxtLink to="pessoa/beyonce">beyoncé</NuxtLink>
+    <h2>Comunidades:</h2>
+    <NuxtLink to="abro-a-geladeira-pra-pensar">Abro a geladeira pra pensar</NuxtLink><br>
+    <NuxtLink to="radio-maloca">Rádio Maloca</NuxtLink>
+  </main>
 </template>
 
 <script>
@@ -11,4 +14,7 @@ export default {}
 </script>
 
 <style scoped>
+a {
+    @apply font-bold;
+}
 </style>
