dddcc72ee62dce4f2cf1493e1aae2feae208c38b
Dandelião <danielramos@tuta.io>
2021-10-05 16:23:36 -0300

    feat: fetch pessoa na api do servidor

--- pages/pessoa/_pessoa.vue
+++ pages/pessoa/_pessoa.vue
@@ -1,6 +1,27 @@
 <template>
-  <main>
-    <p>quem? {{ $route.path }}</p>
-  </main>
+  <main v-if="pagina" v-html="pagina"></main>
 </template>
+
+<script>
+export default {
+  async asyncData(context) {
+    const nome = context.params.pessoa
+    const pessoa = await fetch(`http://localhost:4000/api/pessoas/${nome}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      } else if(res.status === 404) {
+        return Promise.reject(new Error('Erro 404: não encontrado'));
+      } else {
+        return Promise.reject(new Error('Erro: ' + res.status));
+      }
+    })
+    console.log('pessoa', pessoa)
+    const pagina = pessoa.html
+    return {
+      pagina,
+    }
+  },
+}
+</script>
 
