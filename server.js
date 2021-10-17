45f4d1045093ee90408fb826be4934f951240545
Dandelião <danielramos@tuta.io>
2021-10-17 16:06:55 -0300

    fix: corrige referência à pasta assets

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 
-app.use("/assets", express.static(path.resolve("frontend", "static")));
+app.use("/assets", express.static(path.resolve("frontend", "assets")));
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve("frontend", "index.html"));
