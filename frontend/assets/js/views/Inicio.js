e42595eb96fa0473b69fd99ac645df72d923b995
Dandelião <danielramos@tuta.io>
2021-10-19 19:56:50 -0300

    fix: ajusta título de view

--- frontend/assets/js/views/Inicio.js
+++ frontend/assets/js/views/Inicio.js
@@ -3,7 +3,7 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
-        this.setTitle("início");
+        this.setTitle("maloca");
     }
 
     async getHtml() {
