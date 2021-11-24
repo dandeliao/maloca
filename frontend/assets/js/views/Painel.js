d9705863106ee3d307fbd76a52b7876090ef41c1
dandeliao <danielramos@tuta.io>
2021-11-24 17:52:15 -0300

    feat: carrega painel do servidor

--- frontend/assets/js/views/Painel.js
+++ frontend/assets/js/views/Painel.js
@@ -8,80 +8,17 @@ export default class extends AbstractView {
     }
 
     async getHtml() {
-        return `
-        <m-container style="
-            --background-image: linear-gradient(lightpink, #a7f3d0);
-            --container-background: rgba(256, 256, 256, 0.3);
-            ">
-            <m-card>
-                <h3>Meus Perfis:</h3>
-                <br>
-                <div class="info-perfil">
-                    <img src="http://localhost:4000/pessoas/dani/avatar.png">
-                    <div>
-                        <h4><a href="/pessoa/dani" data-link>Dani</a></h4>
-                        <p>Na Maloca há 6 meses</p>
-                        <p>Participa de 22 comunidades</p>
-                    </div>
-                </div>
-                <div class="info-perfil">
-                    <img src="http://localhost:4000/pessoas/rukib/avatar.png">
-                    <div>
-                        <h4><a href="/pessoa/rukib" data-link>Rukib</a></h4>
-                        <p>Na Maloca há 2 meses</p>
-                        <p>Participa de 5 comunidades</p>
-                    </div>
-                </div>
-            </m-card>
-            <m-card>
-                <h3>Minhas Comunidades:</h3>
-                <br>
-                <div class="info-comunidade">
-                    <img src="http://localhost:4000/comunidades/radio-maloca/avatar.png">
-                    <a href="/radio-maloca" data-link>Rádio Maloca</a>
-                    <div style="margin-left: 1em">
-                        <p> | dani (admin, moderação) </p>
-                        <p> | rukib (edição) </p>
-                    </div>
-                </div>
-                <p>...</p>
-            </m-card>
-        </m-container>
-        <style>
-            .info-perfil {
-                display: flex;
-                flex-direction: row;
-                justify-content: start;
-                margin-bottom: 1em;
+        // futuramente permitir que as pessoas editem seus paineis personalizados
+        const painel = await fetch(`http://localhost:4000/api/instancias/maloca/painel`)
+        .then(res => {
+            if (res.ok) {
+                return res.json();
+            } else if(res.status == 404) {
+                return Promise.reject(new Error('Erro 404: não encontrado'));
+            } else {
+                return Promise.reject(new Error('Erro: ' + res.status));
             }
-            .info-perfil > img {
-                max-width: 80px;
-                max-height: 80px;
-                width: 100%;
-                height: auto;
-                border-radius: 100%;
-                margin-right: 2em;
-            }
-
-            .info-comunidade {
-                display: flex;
-                flex-direction: row;
-                justify-content: start;
-                align-items: center;
-            }
-
-            .info-comunidade > img {
-                max-width: 80px;
-                max-height: 80px;
-                width: 100%;
-                height: auto;
-                margin-right: 2rem;
-            }
-
-            ul {
-                margin-left: 2rem;
-            }
-        </style>
-        `;
+        });
+        return painel.html;
     }
 }
