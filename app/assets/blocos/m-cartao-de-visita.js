a8f8bd80a5d504380a002a2c6ee8ee560a85f3d1
dandeliao <danielramos@tuta.io>
2022-11-05 18:52:09 -0300

    alterações de estilo

--- app/assets/blocos/m-cartao-de-visita.js
+++ app/assets/blocos/m-cartao-de-visita.js
@@ -38,10 +38,13 @@ class MCartaoDeVisita extends MalocaElement {
 		let divTexto = document.createElement('div');
 		divTexto.setAttribute('id', 'texto');
 		let mAvatar = document.createElement('m-avatar');
+		let divEspaco = document.createElement('div');
+		divEspaco.setAttribute('id', 'espaco');
 		let mNome = document.createElement('m-nome');
 		let mDescricao = document.createElement('m-descricao');
 
 		divCartao.appendChild(divFundo);
+		divTexto.appendChild(divEspaco);
 		divTexto.appendChild(mNome);
 		divTexto.appendChild(mDescricao);
 		divCartao.appendChild(divTexto);
@@ -49,45 +52,41 @@ class MCartaoDeVisita extends MalocaElement {
 
 		this.style.display = 'block';
 
-		divCartao.style.height = '18rem';
 		divCartao.style.position = 'relative';
 
+		divFundo.style.height = '7.2rem'
 		divFundo.style.backgroundImage = `url('${estado.urlServidor}/${estado.view.tipo}s/${estado.view.id}/objetos/fundo')`;
 		divFundo.style.display = 'block';
 		divFundo.style.position = 'relative';
-		divFundo.style.height = '40%';
 		divFundo.style.width = '100%';
 		divFundo.style.borderBottom = '1px solid #AAAAAA';
 
 		divTexto.style.position = 'relative';
 		divTexto.style.boxSizing = 'border-box';
-		divTexto.style.display = 'grid';
-		divTexto.style.height = '60%';
 		divTexto.style.width = '100%';
-		divTexto.style.gridTemplateColumns = '148px 1fr';
-		divTexto.style.gridTemplateRows = '1fr 2fr';
-		divTexto.style.overflow = 'auto';
+		divTexto.style.textAlign = 'left';
+
+		divEspaco.style.display = 'inline-block';
+		divEspaco.style.width = '132px';
+		divEspaco.style.height = '3.5rem';
+		
+		mNome.style.display = 'inline-block';
+		mNome.style.fontWeight = 'bold';
+		mNome.style.fontSize = '1.6rem';
+		mNome.style.verticalAlign = 'top';
+		mNome.style.padding = '0.68rem 1.5rem';
+
+		mDescricao.style.display = 'block';
+		mDescricao.style.fontSize = '0.96rem';
+		mDescricao.style.textAlign = 'left';
+		mDescricao.style.padding = '0.68rem 1.5rem 1.5rem';
 
 		mAvatar.style.display = 'block';
 		mAvatar.style.position = 'absolute';
 		mAvatar.style.maxWidth = '128px';
-		mAvatar.style.top = '40%';
+		mAvatar.style.top = '7.2rem';
 		mAvatar.style.transform = 'translate(15%, -60%)';
 		mAvatar.style.zIndex = '2';
-
-		mNome.style.fontWeight = 'bold';
-		mNome.style.fontSize = '1.6rem';
-		mNome.style.alignSelf = 'start';
-		mNome.style.justifySelf = 'left';
-		mNome.style.padding = '0.68rem 1.5rem 0 0';
-		mNome.style.gridColumnStart = '2';
-
-		mDescricao.style.fontSize = '0.96rem';
-		mDescricao.style.textAlign = 'left';
-		mDescricao.style.alignSelf = 'start';
-		mDescricao.style.padding = '0.6rem 1.5rem';
-		mDescricao.style.gridColumnStart = '1';
-		mDescricao.style.gridColumnEnd = '3';
 
 		mAvatar.renderizar(estado);
 		mNome.renderizar(estado);
