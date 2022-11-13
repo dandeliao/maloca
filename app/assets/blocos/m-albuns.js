7e97902aa4c21a0962ca51d2e4f3ac7201e4c9cf
dandeliao <danielramos@tuta.io>
2022-11-13 13:00:26 -0300

    implementa visualização de imagens

--- app/assets/blocos/m-albuns.js
+++ app/assets/blocos/m-albuns.js
@@ -59,6 +59,7 @@ class MAlbuns extends MalocaElement {
                 capa = document.createElement('img');
                 capa.setAttribute('src', `http://localhost:4000/${estado.view.tipo}s/${estado.view.id}/objetos/imagem?id=${album.capa_id}`);
                 capa.setAttribute('alt', `capa do álbum ${album.nome}`);
+                capa.setAttribute('title', `capa do álbum ${album.nome}`);
                 capa.style.width = "100%";
 
                 nome = document.createElement('p');
@@ -68,6 +69,86 @@ class MAlbuns extends MalocaElement {
 				botao.appendChild(capa);
                 botao.appendChild(nome);
                 divLista.appendChild(botao);
+
+                // define atributos para facilitar uso no event listener
+                botao.setAttribute('album', album.nome);
+                botao.setAttribute('tipo', estado.view.tipo);
+                botao.setAttribute('localId', estado.view.id);
+
+                botao.addEventListener('click', async e => {
+                    let nomeAlbum = e.currentTarget.getAttribute('album');
+                    let tipoLocal = e.currentTarget.getAttribute('tipo');
+                    let idLocal = e.currentTarget.getAttribute('localId');
+
+                    console.log('álbum clicado:', nomeAlbum);
+                    let res = await serverFetch(`/${tipoLocal}s/${idLocal}/objetos/imagens`, 'GET');
+                    let imagens = await res.json();
+                    let arrayImagens = [];
+
+                    for (let i = 0; i < imagens.length; i++) {
+                        if (imagens[i].album === nomeAlbum) {
+                            arrayImagens.push(imagens[i]);
+                        }
+                    }
+
+                    let overlay = document.createElement('div');
+                    overlay.style.display = "block";
+                    overlay.style.position = "fixed";
+                    overlay.style.zIndex = "2";
+                    overlay.style.width = "100%";
+                    overlay.style.height = "100%";
+                    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+                    overlay.style.left = "0px";
+                    overlay.style.top = "0px";
+                    
+                    let modalImagens = document.createElement('m-bloco');
+                    modalImagens.style.display = "block";
+                    modalImagens.style.position = "absolute";
+                    modalImagens.style.left = "50%";
+                    modalImagens.style.top = "50%";
+                    modalImagens.style.transform = "translate(-50%, -50%)";
+                    modalImagens.style.margin = "0 auto";
+                    modalImagens.style.width = "90%";
+                    modalImagens.style.maxHeight = "90%";
+                    modalImagens.style.zIndex = "3";
+
+                    let elTitulo = document.createElement('h2');
+                    elTitulo.textContent = nomeAlbum;
+                    modalImagens.appendChild(elTitulo);
+                    
+                    arrayImagens.forEach(img => {
+                        let elImg = document.createElement('img');
+                        console.log('imagem:', img);
+                        let imgId;
+                        if (tipoLocal === 'pessoa') {
+                            imgId = img.imagem_pessoal_id;
+                        } else if (tipoLocal === 'comunidade') {
+                            imgId = img.imagem_comunitaria_id;
+                        }
+                        elImg.src = `http://localhost:4000/${tipoLocal}s/${idLocal}/objetos/imagem?id=${imgId}`;
+                        elImg.style.maxWidth = '33rem';
+                        elImg.style.margin = "0.5rem";
+                        elImg.setAttribute('alt', img.descricao);
+                        elImg.setAttribute('title', img.descricao);
+                        console.log('elImg', elImg);
+                        modalImagens.appendChild(elImg);
+                    });
+                    
+                    //modalImagens.appendChild(formAdicionar);
+                    console.log('this', this);
+                    this.appendChild(modalImagens);
+                    this.appendChild(overlay);
+
+                    overlay.addEventListener('click', e => {
+                        e.preventDefault();
+                        while (modalImagens.lastChild) {
+                            modalImagens.removeChild(modalImagens.lastChild);
+                        }
+                        modalImagens.remove();
+                        overlay.remove();
+                    });
+                });
+
             });
 
             divLista.style.display = "flex";
