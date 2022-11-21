33125bb1a7a7067689557a813e816e7cd5b0093d
dandeliao <danielramos@tuta.io>
2022-11-21 13:55:33 -0300

    implementa informações das imagens

--- app/assets/blocos/m-album.js
+++ app/assets/blocos/m-album.js
@@ -53,12 +53,73 @@ class MAlbum extends MalocaElement {
 				elImg.setAttribute('numero', imagemId);
 				elImg.setAttribute(`${tipo}`, id);
 				
-				elBloco.appendChild(elImg);
+				// cria elemento que receberá informações (pessoa que postou, data de postagem, comentários)
+				let elInfo = document.createElement('div');
+								
+				// cria elementos com informações da pessoa que postou
+				let elPessoa = document.createElement('div');
+				let elAvatar = document.createElement('m-avatar');
+				let elDivNomeArroba = document.createElement('div');
+				let elNome = document.createElement('div');
+				let elArroba = document.createElement('a');
+				let res = await serverFetch(`/pessoas/${img.pessoa_id}`, 'GET');
+				let pessoa = await res.json();
+				elAvatar.setAttribute('pessoa', pessoa.nome);
+				elNome.innerText = pessoa.nome;
+				elArroba.innerText = '@' + pessoa.pessoa_id;
+				elArroba.setAttribute('href', `/pessoa/${pessoa.pessoa_id}`);
+				elArroba.setAttribute('data-link', '');
+				elDivNomeArroba.appendChild(elNome);
+				elDivNomeArroba.appendChild(elArroba);
+				elPessoa.appendChild(elAvatar);
+				elPessoa.appendChild(elDivNomeArroba);
 
+				// cria elementos com data da postagem e botão para ver os comentários
+				let elDadosPostagem = document.createElement('div');
+				let elData = document.createElement('div');
+				let elBotaoComentarios = document.createElement('button');
+				elBotaoComentarios.innerText = 'ver comentários';
+				// regex para converter formato de data (timestampz do psql) em DD/MM/AAAA
+				let dataRegex = img.data_criacao.matchAll(/(\d*)(?:-|T)/g);
+				let dataMatch = [];
+				for (const d of dataRegex) {
+					dataMatch.push(d);
+				}
+				elData.innerText = 'postado em ' + dataMatch[2][1] + '/' + dataMatch[1][1] + '/' + dataMatch[0][1];
+				elDadosPostagem.appendChild(elData);
+				elDadosPostagem.appendChild(elBotaoComentarios);
+
+				elInfo.appendChild(elPessoa);
+				elInfo.appendChild(elDadosPostagem);
+
+				// estiliza elementos
+				elInfo.style.display = 'flex';
+				elInfo.style.justifyContent = 'space-between';
+				elInfo.style.alignItems = 'flex-end';
+				elPessoa.style.display = 'flex';
+				elPessoa.style.alignItems = 'center';
+				elPessoa.style.gap = '1rem';
+				elAvatar.redondo = false;
+				elAvatar.style.maxWidth = '64px';
+				elDivNomeArroba.style.textAlign = 'left';
+				elDadosPostagem.style.display = 'flex';
+				elDadosPostagem.style.flexDirection = 'column';
+				elDadosPostagem.style.justifyContent = 'space-between';
+				elDadosPostagem.style.gap = '1rem';
+				//elInfo.style.borderTop = '2px dashed var(--cor-destaque)';
+				elInfo.style.paddingTop = '0.75rem';
+				elImg.style.marginBottom = '1.5rem';
+				elImg.style.width = '100%';
+
+				elBloco.appendChild(elImg);
+				elBloco.appendChild(elInfo);
 				this.appendChild(elBloco);
 				this.appendChild(document.createElement('br'));
-				
+
+				// renderiza blocos
+				elAvatar.renderizar(estado);
 				elImg.renderizar(estado);
+
 			});
 
 		} else {
